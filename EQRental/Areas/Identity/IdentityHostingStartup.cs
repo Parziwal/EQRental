e2c45efe62da56867ce3959f8f0de878def06647
Stedra Kristóf <stedra99@gmail.com>
2020-10-15 19:37:51 +0200

    Nuget package update

---
+++ EQRental/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EQRental.Data;
+using EQRental.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EQRental.Areas.Identity.IdentityHostingStartup))]
+namespace EQRental.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
