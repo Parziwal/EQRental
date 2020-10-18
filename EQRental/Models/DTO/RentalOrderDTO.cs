63130c76503dfe66d4f6c5618f0c517fa5041de4
bognargabor99 <gaborbognar99@gmail.com>
2020-10-18 19:02:29 +0200

    Async EquipmentController with Rental posting option

---
+++ EQRental/Models/DTO/RentalOrderDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EQRental.Models.DTO
+{
+    public class RentalOrderDTO
+    {
+        public int EquipmentId { get; set; }
+        public int AddressId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
