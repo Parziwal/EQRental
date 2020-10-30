5db41fac20f3849b15e25d778ffb7aa5639f0896
Stedra Kristóf <stedra99@gmail.com>
2020-10-30 16:04:28 +0100

    Make frontend structure

--- EQRental/ClientApp/src/app/app.module.ts
+++ EQRental/ClientApp/src/app/app.module.ts
@@ -2,35 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
-import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
+import { RentalComponent } from './rental/rental.component';
+import { MyEquipmentsComponent } from './my-equipments/my-equipments.component';
+import { RentedEquipmentsComponent } from './rented-equipments/rented-equipments.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    RentalComponent,
+    MyEquipmentsComponent,
+    RentedEquipmentsComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    ApiAuthorizationModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
-    ])
+    ApiAuthorizationModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true }
---
+++ EQRental/ClientApp/src/app/rented-equipments/rented-equipments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rented-equipments',
+  templateUrl: './rented-equipments.component.html',
+  styleUrls: ['./rented-equipments.component.css']
+})
+export class RentedEquipmentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
